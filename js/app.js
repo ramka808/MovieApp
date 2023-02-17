f8dbf47c14f174cefda7ff95668dbfe052b0c9c5
Ramil Yafarov <ramka808@yandex.ru>
2023-02-17 17:31:30 +0300

    add modal window

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const API_URL_POPULAR =
 	'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1'
 const API_URL_SEARCH =
 	'https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword='
+const API_URL_MOVIE_DETAIS =
+	'https://kinopoiskapiunofficial.tech/api/v2.2/films/'
 
 getMovies(API_URL_POPULAR)
 
@@ -62,6 +64,7 @@ function showMovies(data) {
 					}
       </div>
     `
+		movieEl.addEventListener('click', () => openModal(movie.filmId))
 		moviesEl.appendChild(movieEl)
 	})
 }
@@ -75,5 +78,65 @@ form.addEventListener('submit', (e) => {
 	if (search.value) {
 		getMovies(apiSearchUrl)
 		search.value = ''
+	}
+})
+
+//Modal
+const modalEl = document.querySelector('.modal')
+
+async function openModal(id) {
+	const resp = await fetch(API_URL_MOVIE_DETAIS + id, {
+		headers: {
+			'Content-Type': 'application/json',
+			'X-API-KEY': API_KEY
+		}
+	})
+	const respData = await resp.json()
+
+	modalEl.classList.add('modal--show')
+	document.body.classList.add('stop-scrolling')
+	modalEl.innerHTML = `
+<div class="modal__card">
+<img class="modal__movie-backdrop" src="${respData.posterUrl}" alt="">
+<h2>
+	<span class="modal__movie-title">${respData.nameRu}</span>
+	<span class="modal__movie-release-year"> - ${respData.year}</span>
+</h2>
+<ul class="modal__movie-info">
+	<div class="loader"></div>
+	<li class="modal__movie-genre">Жанр - ${respData.genres.map(
+		(el) => `<span>${el.genre}</span>`
+	)}</li>
+	${
+		respData.filmLength
+			? `<li class="modal__movie-runtime">Время - ${respData.filmLength} минут</li>`
+			: ''
+	}
+	<li >Сайт: <a class="modal__movie-site" href="${respData.webUrl}">${
+		respData.webUrl
+	}</a></li>
+	<li class="modal__movie-overview">Описание - ${respData.description}</li>
+</ul>
+<button type="button" class="modal__button-close">Закрыть</button>
+</div>
+`
+	const btnClose = document.querySelector('.modal__button-close')
+	btnClose.addEventListener('click', () => closeModal())
+}
+
+function closeModal() {
+	modalEl.classList.remove('modal--show')
+	document.body.classList.remove('stop-scrolling')
+}
+
+window.addEventListener('click', (e) => {
+	if (e.target === modalEl) {
+		closeModal()
+	}
+})
+
+window.addEventListener('keydown', (e) => {
+	if (e.keyCode === 27) {
+		closeModal()
 	}
 })
